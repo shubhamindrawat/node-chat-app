e576ae93bb7718b5ce1f48e7bf9d0d8b6ddd9cd9
Shubham Jain <shubhamj@aosmithindia.com>
2020-06-07 18:26:57 +0530

    Add CSS for chat app

--- public/js/index.js
+++ public/js/index.js
@@ -5,7 +5,7 @@ socket.on('connect', function(){
 
 socket.on('newMessage', function(message){
     console.log('newMessage', message);
-    var li = $('<li></li>');
+    var li = $('<div></div>');
     li.text(`${message.from}: ${message.text}`);
     $('#messages').append(li);
 });
@@ -19,40 +19,42 @@ socket.on('newEmail', function(email){
 });
 
 socket.on('newLocationMessage',  function(message){
-    var li = $('<li></li>');
+    var li = $('<div></div>');
     var a = $('<a target="_BLANK">My Current Location</a>');
-    li.text(`${message.from}`);
+    li.text(`${message.from}: `);
     a.attr('href', message.url)
     li.append(a);
     $('#messages').append(li);
 });
 
 $('#message-form').on('submit', function(e){
+    var messageTextbox = $('[name=message]');
     e.preventDefault();
     socket.emit('createMessage', {
         from: 'User',
-        text: $('[name=message]').val()
+        text: messageTextbox.val()
     }, function(){
-        
+        messageTextbox.val('');
     });
 });
 
 var locationButton = $('#sendLocation');
 
 locationButton.on('click', function(){
-    
     if(!navigator.geolocation){
         return alert('Geolocation not supported by your browser');
     }
 
-    navigator.geolocation.getCurrentPosition(function(position){
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
 
+    navigator.geolocation.getCurrentPosition(function(position){
+        locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
-        })
+        });
     }, function(){
+        locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fetch your location');
     });
-    
 });
